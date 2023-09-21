36b7897e1be3401df80d268a5ad40bb955b07ce7
Nguyễn Ngọc Ánh <122151638+Pearl2612@users.noreply.github.com>
2023-09-21 20:58:08 +0700

    Make sidebar item active depending on page

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -1,2 +1,13 @@
 <?php
 
+/** Set Sideban item active */
+
+function setActive(array $route) {
+    if(is_array($route)) {
+        foreach($route as $r) {
+            if(Request()->routeIs($r)) {
+                return 'active';
+            }
+        }
+    }
+}
